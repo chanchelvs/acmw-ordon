08eedc234acd580de09d95d1ce6ef8c4a001dee9
Suchithra <vssuchithra3@gmail.com>
2017-10-02 19:43:04 +0530

    Fixed Urls

--- ordon/urls.py
+++ ordon/urls.py
@@ -8,7 +8,10 @@ urlpatterns =[
     # url(r'^$', 'ordon.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.welcome, name='welcome'),
+    url(r'^home/$', views.home, name='home'),
     url(r'^faq/$', views.faq, name='faq'),
     url(r'^firstaid/$', views.firstaid, name='firstaid'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 ]
